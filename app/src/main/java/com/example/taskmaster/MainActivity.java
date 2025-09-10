264d574d24081e363eef149a6066ce99e8fd529a
miton56 <simonbarrosquepasa@gmail.com>
2025-09-10 16:11:10 -0300

    inicial

---
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.taskmaster;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Button botonCrearTareas = findViewById(R.id.btnCrearTarea);
+        Button botonAlarma = findViewById(R.id.btnCambiarAlarma);
+        Button botonVerTareas = findViewById(R.id.btnVerTarea);
+
+        botonCrearTareas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, crearTareas.class);
+                startActivity(intent);
+            }
+        });
+
+        botonAlarma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CambiarAlarma.class);
+                startActivity(intent);
+            }
+        });
+
+        botonVerTareas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, verTareas.class);
+                startActivity(intent);
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+
+}
